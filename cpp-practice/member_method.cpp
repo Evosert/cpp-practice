5265e25faa1e21ec11f4c34dfe2d38afd130ffc7
Vito <kztsly260@gmail.com>
2022-11-15 16:29:05 +0900

    [member_method] 생성자와 멤버 메서드 선언, 정의 분리

--- cpp-practice/member_method.cpp
+++ cpp-practice/member_method.cpp
@@ -5,14 +5,19 @@ using namespace std;
 class Vector2
 {
 public:
-	Vector2() : x(0), y(0) {}
-	Vector2(float x, float y) : x(x), y(y) {}
+	Vector2();
+	Vector2(float x, float y);
 
-	float GetX() const { return x; }
-
-	float GetY() const { return y; }
+	float GetX() const;
+	float GetY() const;
 
 private:
 	float x;
 	float y;
 };
+
+Vector2::Vector2() : x(0), y(0) {}
+Vector2::Vector2(float x, float y) : x(x), y(y) {}
+
+float Vector2::GetX() const { return x; }
+float Vector2::GetY() const { return y; }
